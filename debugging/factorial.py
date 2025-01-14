27fa14ca20a6dc948d15c9e930cdbecf9da50cf4
sira-djam <b.soileux@gorange.fr>
2025-01-14 14:54:17 +0100

    factorial.py modification du nom et du code

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -5,7 +5,7 @@ def factorial(n):
     result = 1
     while n > 1:
         result *= n
-        n -= 1
+        n -= 1  # Décrémenter n à chaque itération
     return result
 
 f = factorial(int(sys.argv[1]))
